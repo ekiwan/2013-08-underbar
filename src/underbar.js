1b89c6b9b926458432e174bdb5fa91255f071ad6
ekiwan <elie.kiwan@gmail.com>
2013-07-22 13:19:24 -0700

    fixed last

--- src/underbar.js
+++ src/underbar.js
@@ -25,6 +25,16 @@ var _ = { };
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+     if (n === undefined) {
+       return array.pop();
+    }
+     else if (n > array.length) {
+       return array.slice(0, array.length);
+    }
+     else {
+       return array.slice(array.length - n, array.length);
+     }
+
   };
 
   // Call iterator(value, key, collection) for each element of collection.
