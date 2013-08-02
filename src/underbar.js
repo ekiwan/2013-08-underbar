f8aab1e010efcf6a7c6e1ffa38844f5e1a6694be
ekiwan <elie.kiwan@gmail.com>
2013-08-01 22:10:43 -0700

    added delay but i can't get the args to be passed to the function

--- src/underbar.js
+++ src/underbar.js
@@ -348,6 +348,12 @@ var _ = { };
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    
+    var args = Array.prototype.slice.apply(arguments).slice(2); //gets the args
+   
+    window.setTimeout(function() { //calls function after timeout
+      func.apply(args); //I can't get the args to apply
+    }, wait);
   };
 
 
